5b53a3f5f2cc2761001d7b64faae7a4db5e8e573
Stephen MacDonald <156457824+StephenFleming30@users.noreply.github.com>
2024-01-23 10:30:07 -0500

    Step 4
    Fixed main and add print_student_id function.

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ def main():
         "fullname": "Stephen MacDonald",
         "student_id": 10308012,
         "pizza_toppings": ["PEPPERONI", "SPINACH", "BEEF"],
-        "movies": [{"hacksaw ridge": "action"}, 
-                   {"plane": "action"},
-                   {"the hunger games": "action and adventure"}
+        "movies": [{"title" :"hacksaw ridge", "genre": "action"}, 
+                   {"title": "plane", "genre": "action"},
                    ]
     }
+
+    about_me["movies"].append({"title": "the hunger games", "genre": "action and adventure"})
+    print_student_id(about_me)
+
+def print_student_id(about_me):
+    fullname = about_me["fullname"]
+    last_name = about_me["fullname"].split(" ")
+    student_id = about_me["student_id"]
+    print(f"My name is {fullname}, but you can call me Sir {last_name[0]}.")
+    print(f"My student ID is {student_id}")
 
 if __name__ == "__main__":
     main()
