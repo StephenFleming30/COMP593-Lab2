d7c405766ad457700110748c74ec71333a0a436f
Stephen MacDonald <156457824+StephenFleming30@users.noreply.github.com>
2024-01-23 10:45:55 -0500

    Step 5
    Created add_pizza_toppings function fully finished.

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def main():
     }
 
     about_me["movies"].append({"title": "the hunger games", "genre": "action and adventure"})
+    pizza_toppings = ("pinnaple", "bacon")
     print_student_id(about_me)
+    add_pizza_toppings(about_me, pizza_toppings)
 
 def print_student_id(about_me):
     fullname = about_me["fullname"]
@@ -17,6 +19,13 @@ def print_student_id(about_me):
     student_id = about_me["student_id"]
     print(f"My name is {fullname}, but you can call me Sir {last_name[0]}.")
     print(f"My student ID is {student_id}")
+
+def add_pizza_toppings(about_me, pizza_toppings):
+    for topping in pizza_toppings:
+        about_me["pizza_toppings"].append(topping)
+    about_me["pizza_toppings"].sort()
+    about_me["pizza_toppings"] = [topping.casefold() for topping in about_me["pizza_toppings"]]
+    return about_me["pizza_toppings"]
 
 if __name__ == "__main__":
     main()
