4db51e36b47b9258e1ac4f387e6d3472bd88c512
Stephen MacDonald <156457824+StephenFleming30@users.noreply.github.com>
2024-01-23 11:12:52 -0500

    Step 7
    Added function for comma seperated list of movie genres.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     print_bullet_list(about_me)
     add_pizza_toppings(about_me, pizza_toppings)
     print_bullet_list(about_me)
+    list_of_movies_genres(about_me)
 
 def print_student_id(about_me):
     fullname = about_me["fullname"]
@@ -33,6 +34,14 @@ def print_bullet_list(about_me):
     print("\nMy favourite pizza toppings are:")
     for topping in about_me["pizza_toppings"]:
         print(f"- {topping}")
+
+def list_of_movies_genres(about_me):
+    genres = []
+    count = 0
+    for _ in range(len(about_me["movies"]) - 1):
+        genres.append(about_me["movies"][count]["genre"])
+        count += 1
+    print(f"\nI like to watch {', '.join(genres)}")
 
 if __name__ == "__main__":
     main()
