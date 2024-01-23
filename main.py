67f823bdd848496f1602a125f6583ed0601e708a
Stephen MacDonald <156457824+StephenFleming30@users.noreply.github.com>
2024-01-23 10:20:22 -0500

    Create main
    Created main with data structure

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    about_me = {
+        "fullname": "Stephen MacDonald",
+        "student_id": 10308012,
+        "pizza_toppings": ["PEPPERONI", "SPINACH", "BEEF"],
+        "movies": [{"hacksaw ridge": "action"}, 
+                   {"plane": "action"},
+                   {"the hunger games": "action and adventure"}
+                   ]
+    }
+
+if __name__ == "__main__":
+    main()
